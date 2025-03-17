d383d556b0ec74eae420e8d2a031d3e82898f3ca
Dilukshan Viyapury <Dviyapury@gmail.com>
2025-03-17 20:10:01 +0530

    Updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import RecoveryPage from "./pages/Recovery_email.jsx";
 import RecoveryOTP from "./pages/Recovery_OTP.jsx";
 import RecoveryPassword from "./pages/Recovery_Password.jsx";
 import Unauthorized from "./pages/Unauthorized.jsx";
-import UserHome from './pages/Home.jsx'
+import UserHome from './pages/Home.jsx';
 import UserDashboard from "./pages/Dashboard.jsx";
 import AdminDashboard from "./pages/Admin_Dashboard.jsx";
 import AddEmployeeForm from "./pages/Add_employee.jsx";
@@ -22,7 +22,8 @@ import Customers from "./pages/Dilukshan/Customers.jsx";
 import Services from "./pages/Dilukshan/Services.jsx";
 import Vehicles from "./pages/Dilukshan/Vehicles.jsx";
 import Analytics from "./pages/Dilukshan/Analytics.jsx";
-import Settings from './pages/Dilukshan/Settings.jsx'
+import Settings from './pages/Dilukshan/Settings.jsx';
+
 
 function App() {
   return (
@@ -31,16 +32,18 @@ function App() {
         <Header />
         <main className="flex-grow">
           <Routes>
+            {/* Public Routes */}
             <Route path="/" element={<UserHome />} />
-            <Route path="/otp" element={<OTP />} />
             <Route path="/sign-up" element={<Signup />} />
             <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/otp" element={<OTP />} />
             <Route path="/recovery-email" element={<RecoveryPage />} />
             <Route path="/recovery-otp" element={<RecoveryOTP />} />
             <Route path="/recovery-password" element={<RecoveryPassword />} />
             <Route path="/employee-signin" element={<EmployeeSignin />} />
             <Route path="/unauthorized" element={<Unauthorized />} />
             <Route path="/add-employee" element={<AddEmployeeForm />} />
+
             <Route element={<PrivateRoute />}>
               <Route path="/dashboard/profile" element={<UserDashboard />} />
               <Route path="/admin-user" element={<AdminDashboard />} />
@@ -56,7 +59,6 @@ function App() {
             <Route path="/vehicles" element={<Vehicles />} />
             <Route path="/analytics" element={<Analytics />} />
             <Route path="/settings" element={<Settings />} />
-            <Route path="/Manager" element={<Manager_Dashboard />} />
 
           </Routes>
         </main>
