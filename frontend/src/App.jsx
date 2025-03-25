7c7be9d8af013a166d1de4458559cddab6f56c76
Dilukshan Viyapury <162113618+Dilukshan285@users.noreply.github.com>
2025-03-25 16:59:42 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,16 +19,12 @@ import EmployeeSignin from "./pages/Employee_SignIn.jsx";
 
 // Dilukshan
 import Manager_Dashboard from "./pages/Dilukshan/Manager_Dashboard.jsx";
-
-
 import Appointments from "./pages/Dilukshan/Appointments.jsx";
-
 import Workers from "./pages/Dilukshan/Workers.jsx";
 import Customers from "./pages/Dilukshan/Customers.jsx";
 import Services from "./pages/Dilukshan/Services.jsx";
 import Vehicles from "./pages/Dilukshan/Vehicles.jsx";
 import Analytics from "./pages/Dilukshan/Analytics.jsx";
-
 
 // Venushan's Pages
 import ServiceDashboard from "./pages/venushan/ServiceDashboard.jsx";
@@ -36,24 +32,6 @@ import ServiceDetails from "./pages/venushan/ServiceDetails.jsx";
 import AboutUs from "./pages/venushan/Aboutuspage.jsx";
 import BookingForm from "./pages/venushan/AppoinmentBookingimg.jsx";
 import ImageUploadForm from "./pages/venushan/AppoinmentBooking.jsx";
-
-
-// Raagul Gananathan's Pages
-import ProductTable from "./pages/Raagul/ProductTable.jsx";
-import ProductPage from "./pages/Raagul/ProductPage.jsx";
-import ProductDetail from "./pages/Raagul/ProductDetail.jsx";
-import MyOrdersPage from "./pages/Raagul/MyOrdersPage.jsx";
-import CartPage from "./pages/Raagul/CartPage.jsx";
-import CheckoutPage from "./pages/Raagul/CheckoutPage.jsx";
-import PaymentPage from "./pages/Raagul/PaymentPage.jsx";
-import OrderManagementPage from "./pages/Raagul/OrderManagementPage.jsx";
-import ReviewManagementPage from "./pages/Raagul/ReviewManagementPage.jsx";
-
-// Venushan's வேர்
-import ServiceDashboard from './pages/ServiceDashboard.jsx';
-import ServiceDetails from './pages/ServiceDetails.jsx';
-import AboutUs from './pages/Aboutuspage.jsx';
-
 
 // Raagul Gananathan's வேர்
 import ProductTable from './pages/Raagul/ProductTable.jsx';
@@ -92,7 +70,6 @@ function App() {
 
 
             {/* Protected Routes */}
-=======
 
             <Route element={<PrivateRoute />}>
               <Route path="/dashboard/profile" element={<UserDashboard />} />
@@ -114,17 +91,6 @@ function App() {
             {/* Dilukshan's Routes */}
             <Route path="/manager_dashboard" element={<Manager_Dashboard />} />
 
-            <Route path="/Product" element={< ProductTable/>} />
-            <Route path="/ProductDisplay" element={< ProductPage/>} />
-            <Route path="/ProductDetails/:id" element={< ProductDetail/>} />
-            <Route path="/myorders" element={< MyOrdersPage/>} />
-            <Route path="/cart" element={< CartPage/>} />
-            <Route path="/checkout" element={< CheckoutPage/>} />
-            <Route path="/payment" element={< PaymentPage/>} />
-            <Route path="/adminOrder" element={< OrderManagementPage/>} />
-            <Route path="/adminReview" element={< ReviewManagementPage/>} />
-
-
 
             <Route path="/manager_dashboard" element={<Manager_Dashboard />} />
             <Route path="/appointments" element={<Appointments />} />
@@ -137,7 +103,7 @@ function App() {
 
 
             {/* Venushan's Routes */}
-            <Route path="/appointments" element={<BookingForm />} />
+            <Route path="/booking" element={<BookingForm />} />
             <Route path="/appointment" element={<ImageUploadForm />} />
           
 
